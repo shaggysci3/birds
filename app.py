c96a861c536f72b8718fc640df3ae91af16015f7
matthew <shaggysci3@gmail.com>
2024-06-23 09:30:53 -0400

    adding specificity to the cors origin control

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.json.compact = False
 CORS(app)
 
 # Initialize SocketIO
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="http://127.0.0.1:5173/cart")
 
 migrate = Migrate(app, db)
 db.init_app(app)
