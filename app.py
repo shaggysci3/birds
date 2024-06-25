1096a58ac35b9466add92a2dfe4bfac7fac7f5a4
matthew <shaggysci3@gmail.com>
2024-06-24 20:58:26 -0400

    made post request function for the api post man says it worked idk if it did tho got a 201 meaning something was pushed

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from flask import request
+from flask import Flask, make_response, jsonify, request
 
-from models import db, Bird
+from models import db, Bird,Products
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
@@ -26,6 +28,48 @@ class Birds(Resource):
         return make_response(jsonify(birds), 200)
 
 api.add_resource(Birds, '/birds')
+
+
+
+class AllProducts(Resource):
+
+    def get(self):
+        response_body = [product.to_dict() for product in Products.query.all()]
+        return make_response(response_body,200)
+    def post(self):
+        try:
+            
+            # Ensure required fields are present in the request
+            # id = request.json.get('id')
+            img = request.json.get('img')
+            name = request.json.get('name')
+            price = request.json.get('price')
+            info = request.json.get('info')
+
+            if not all([img,name,price , info]):
+                raise ValueError("Missing required fields")
+
+            new_p = Products(
+                
+                img=img,
+                name=name,
+                price=price,
+                info=info,
+            )
+            db.session.add(new_p)
+            db.session.commit()
+
+            # Assuming to_dict() method is defined in your Mission model
+            rb = new_p.to_dict(rules = ())
+            return make_response(rb, 201)
+
+        except ValueError:
+            rb = {
+                "errors": ["validation errors"]
+                }
+            return make_response(rb, 400)
+
+api.add_resource(AllProducts, '/products')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -12,3 +12,15 @@ class Bird(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'<Bird {self.name} | Species: {self.species}>'
+    
+class Products(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.String)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    info = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Products {self.name} | {self.id}>'
